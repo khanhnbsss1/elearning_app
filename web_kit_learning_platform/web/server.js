d57c9c7d62cde708ef04a2b09db16e594e51511c
fftresource <fftresource@gmail.com>
2024-08-03 22:13:38 -0700

    update config file

--- web_kit_learning_platform/web/server.js
+++ web_kit_learning_platform/web/server.js
@@ -1,12 +1,15 @@
 // Define the variables
 const express = require('express');
+var path = require('path');
 const app = express();
-const PORT = 60000;
-const HOST = '0.0.0.0';
-
-// Serve static files from the project directory
-app.use(express.static('.'));
-
+const PORT = 9090
+const HOST = '192.168.1.234';
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../web')));
+app.get('*', (_, res) => {
+  res.sendFile(path.resolve(__dirname, '../web/index.html'));
+});
 // Listen to the network
 app.listen(PORT, HOST, () => {
     console.log(`Server running on port http://${HOST}:${PORT}`);
