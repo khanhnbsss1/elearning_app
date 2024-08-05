bf032d9f8077e5364d4fe422382b26e356137b9e
fftresource <fftresource@gmail.com>
2024-08-04 18:21:32 -0700

    update deploy server file

--- web_kit_learning_platform/web/server.js
+++ web_kit_learning_platform/web/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 var path = require('path');
 const app = express();
 const PORT = 9090
-const HOST = '192.168.1.234';
+const HOST = '0.0.0.0';
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../web')));
