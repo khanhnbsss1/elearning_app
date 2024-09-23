7fe27edc1d9b0efb60a511f4de698f6306c4cd6b
khanhnbsss1 <khanhnbsss1@gmail.com>
2024-09-23 15:23:27 +0700

    update

--- prime-lms-flutter-elearning-mobile-app/main_files/source/lms_app/lib/screens/tabs/profile_tab/profile_tab.dart
+++ prime-lms-flutter-elearning-mobile-app/main_files/source/lms_app/lib/screens/tabs/profile_tab/profile_tab.dart
@@ -3,12 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lms_app/screens/tabs/profile_tab/settings.dart';
 import 'package:lms_app/screens/tabs/profile_tab/user_info2.dart';
-import '../../../models/user/UserProfile.dart';
-import '../../../providers/user_data_provider.dart';
-import '../../../services/apis/user/get_user_detail_api.dart';
 import '../../../theme/theme_provider.dart';
-import 'guest_user.dart';
-import 'user_info.dart';
 
 class ProfileTab extends ConsumerWidget {
   const ProfileTab({super.key});
@@ -19,8 +14,11 @@ class ProfileTab extends ConsumerWidget {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
-          backgroundColor: (isDarkMode != true) ? Colors.white : Colors.black.withOpacity(0.1),
-          title: const Text('profile').tr(),
+          backgroundColor: Theme.of(context).primaryColor,
+          title: Text('profile', style: Theme.of(context).textTheme.titleLarge?.copyWith(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),).tr(),
           pinned: true,
           titleTextStyle: Theme.of(context)
               .textTheme
